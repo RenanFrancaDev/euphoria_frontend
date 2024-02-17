62290cef95be4230c2205d94ba1095c434738223
Renan Franca <renanfranca.dev@gmail.com>
2024-02-16 23:22:24 -0300

    feat: page home created with promo banner

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import Banner from "../../../public/fotos/banner_promo.png";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <Image
+        height={0}
+        width={0}
+        className=" my-5 h-auto w-full max-h-[500px] px-10 m-auto"
+        sizes="80vw"
+        src={Banner}
+        alt="promo banner"
+      />
+    </div>
+  );
 };
 
 export default Home;
