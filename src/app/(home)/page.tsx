ec60232aea322ee34a8e0ba990a3708a29a42f9a
Renan Franca <renanfranca.dev@gmail.com>
2024-02-19 23:39:43 -0300

    chore: component Banner created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
-import Banner from "../../../public/fotos/banner_promo.png";
 import Categories from "./components/Categories";
+import Banner from "./components/Banner";
 
 const Home = () => {
   return (
     <div className="w-full">
-      <Image
-        height={0}
-        width={0}
-        className=" my-5 h-auto w-full max-h-[500px] px-10 m-auto"
-        sizes="80vw"
-        src={Banner}
-        alt="promo banner"
+      <Banner
+        src="../../../public/fotos/banner_promo.png"
+        alt="Banner inicial"
       />
 
       <Categories />
