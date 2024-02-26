406afc94cd77281a1a2fd341f242615d25d78713
Renan Franca <renanfranca.dev@gmail.com>
2024-02-26 20:16:51 -0300

    fix: Discount badge with props type

--- src/components/Discount-Badge.tsx
+++ src/components/Discount-Badge.tsx
@@ -2,10 +2,14 @@ import { ArrowDownIcon } from "lucide-react";
 import { Badge, BadgeProps } from "./ui/badge";
 import { twMerge } from "tailwind-merge";
 
-const DiscountBadge = ({ children, className, ...props }: BadgeProps) => {
+interface BagdeDicount extends BadgeProps {
+  children: number;
+}
+
+const DiscountBadge = ({ children, className, ...props }: BagdeDicount) => {
   return (
     <Badge className={twMerge("px-2 py-[2px]", className)} {...props}>
-      <ArrowDownIcon size={14} /> {children}%
+      <ArrowDownIcon size={14} /> {children * 100}%
     </Badge>
   );
 };
