a8390e14397af1af1a8c775a98e395beac0d44db
Renan Franca <renanfranca.dev@gmail.com>
2024-02-27 15:15:14 -0300

    fix: auth routes protected by nextAuth, users logged can not acess sig-in page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,20 @@
+import { getServerSession } from "next-auth";
 import { FC, ReactNode } from "react";
+import { nextAuthOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
-const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
-  return <div className="bg-slate-100 p-10 rounded-md">{children}</div>;
+const AuthLayout: FC<AuthLayoutProps> = async ({ children }) => {
+  const session = await getServerSession(nextAuthOptions);
+
+  if (session) {
+    redirect("/");
+  }
+
+  return <>{children}</>;
 };
 
 export default AuthLayout;
