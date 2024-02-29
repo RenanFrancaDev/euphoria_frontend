a6aba273f0cf783de992f7af7856bff824026901
Renan Franca <renanfranca.dev@gmail.com>
2024-02-29 15:35:55 -0300

    feat: Cart Provider created and put into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import NextAuthSessionProvider from "./providers/sessionProvider";
+import CartProvider from "./providers/cartProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthSessionProvider>
-          <Header />
-          {children}
+          <CartProvider>
+            <Header />
+            {children}
+          </CartProvider>
         </NextAuthSessionProvider>
       </body>
     </html>
---
+++ src/app/providers/cartProvider.tsx
@@ -0,0 +1,136 @@
+"use client";
+import { ICartContext } from "@/lib/cartContextProps";
+import { ICartProduct } from "@/lib/cartProductProps";
+import { ReactNode, createContext, useEffect, useMemo, useState } from "react";
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  cartTotalPrice: 0,
+  cartBasePrice: 0,
+  cartTotalDiscount: 0,
+  total: 0,
+  subtotal: 0,
+  totalDiscount: 0,
+  addProductToCart: () => {},
+  decreaseProductToCart: () => {},
+  increaseProductToCart: () => {},
+  deleteProductToCart: () => {},
+});
+
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<ICartProduct[]>([]);
+
+  useEffect(() => {
+    const item = JSON.parse(
+      localStorage.getItem("@euphoria-store/cart-products") || "[]"
+    );
+    // console.log(item)
+    setProducts(item);
+  }, []);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      // console.log("debug"+products)
+      localStorage.setItem(
+        "@euphoria-store/cart-products",
+        JSON.stringify(products)
+      );
+    }
+  }, [products]);
+
+  // Total without discount
+  const subtotal = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  // Total with discount
+  const total = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscount = subtotal - total;
+
+  const addProductToCart = (product: ICartProduct) => {
+    // if product already contains in the cart
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id
+    );
+
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+
+          return cartProduct;
+        })
+      );
+
+      return;
+    }
+    setProducts((prev) => [...prev, product]);
+  };
+
+  const decreaseProductToCart = (productId: string) => {
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId)
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0)
+    );
+  };
+
+  const increaseProductToCart = (productId: string) => {
+    setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        return cartProduct;
+      })
+    );
+  };
+
+  const deleteProductToCart = (productId: string) => {
+    setProducts((prev) =>
+      prev.filter((cartProduct) => cartProduct.id !== productId)
+    );
+  };
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductToCart,
+        increaseProductToCart,
+        deleteProductToCart,
+        total,
+        subtotal,
+        totalDiscount,
+        cartTotalPrice: 0,
+        cartBasePrice: 0,
+        cartTotalDiscount: 0,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
