3fb1810e31e38401d746af6de9ca5a817fe3be5a
Renan Franca <renanfranca.dev@gmail.com>
2024-03-11 20:55:44 -0300

    chore: join the product and products prop in one file

--- src/lib/productProps.ts
+++ src/lib/productProps.ts
@@ -1,16 +1,22 @@
+export interface IProduct {
+  id: string;
+  category_id: string;
+  name: string;
+  description: string;
+  price: number;
+  newPrice: number;
+  discount: number;
+  imageUrls: [string];
+  quantity_stock: number;
+  category: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
 export interface ProductListProps {
-  product: {
-    id: string;
-    category_id: string;
-    name: string;
-    description: string;
-    price: number;
-    newPrice: number;
-    discount: number;
-    imageUrls: [string];
-    quantity_stock: number;
-    category: string;
-    created_at: Date;
-    updated_at: Date;
-  };
+  product: IProduct;
+}
+
+export interface ProductsListProps {
+  products: IProduct;
 }
